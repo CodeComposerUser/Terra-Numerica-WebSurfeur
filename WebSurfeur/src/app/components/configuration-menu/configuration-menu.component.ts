d93bafc7726b6a2be7db6c3b98eaef9b72d8aa16
Gregory-Hoareau <66921801+Gregory-Hoareau@users.noreply.github.com>
2021-06-01 14:49:27 +0200

    #2 Amélioration du menu de configuration
    -ajout de la sélection d'élément dans les différents menu
    -mise en place de limite sur les éléments input
    -Retrait d'éléments en dépendant des sélections faite

--- WebSurfeur/src/app/components/configuration-menu/configuration-menu.component.ts
+++ WebSurfeur/src/app/components/configuration-menu/configuration-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ParamBoundary } from 'src/app/models/param-boundary.model';
 import { GameService } from 'src/app/services/game/game.service';
 
 @Component({
@@ -8,13 +10,144 @@ import { GameService } from 'src/app/services/game/game.service';
 })
 export class ConfigurationMenuComponent implements OnInit {
 
-  constructor(private gameService: GameService) { }
+  public configurations = ['tree', 'conf2', 'conf3'];
+  public selected_configuration: string = 'tree';
+  public configuration_param_boundaries: { [index: string] : {param1: ParamBoundary, param2: ParamBoundary | undefined}} = {
+    tree: {
+      param1: {min: 2, max: 10},
+      param2: {min: 1, max: 10}
+    },
+    conf2: {
+      param1: {min: 2, max: 5},
+      param2: undefined
+    },
+    conf3: {
+      param1: {min: 2, max: 5},
+      param2: {min: 1, max: 10}
+    }
+  }
+  public param1: number = 0;
+  public param2: number = 0;
+
+  public opponent_types = ['ai', 'player'];
+  private selected_opponent_type = 'player' // 'ai';
+
+  public sides = ['goat', 'harvest'];
+  private player_side = 'goat';
+
+  constructor(private gameService: GameService, private router: Router) { }
 
   ngOnInit(): void {
+    this.initParams();
+  }
+
+  private initParams(): void {
+    this.param1 = this.configuration_param_boundaries[this.selected_configuration].param1.min;
+    if(this.configuration_param_boundaries[this.selected_configuration].param2 !== undefined) {
+      this.param2 = this.configuration_param_boundaries[this.selected_configuration].param2!.min;
+    } else { this.param2 = -1 }
   }
 
   displayRules() {
     this.gameService.displayRules()
+  }
+
+  /* Functions for board configuration selection */
+
+  getConfigurationName(configuration: string): string {
+    switch (configuration) {
+      case 'tree':
+        return 'Arbre';
+      case 'conf2':
+        return 'Configuration 2';
+      case 'conf3':
+        return 'Configuration 3';
+      default:
+        return 'Configuration inconnue';
+    }
+  }
+
+  selectConfiguration(configuration: string) {
+    this.selected_configuration = configuration;
+    this.initParams();
+  }
+
+  isSelectedConfiguration(configuration: string): string {
+    return this.selected_configuration === configuration ? 'selected' : ''
+  }
+
+  /* Functions for inputs boundaries */
+
+  checkValueRightness(event: FocusEvent) {
+    const target = event.target as any
+    if(target.value !== '') {
+      if(+target.value < target.min) {
+        target.value = target.min;
+      } else if(target.max !== '' && +target.value > target.max) {
+        target.value = target.max;
+      }
+    } else {
+      target.value = target.min;
+    }
+  }
+
+  /* Functions for opponent type selection */
+
+  getOpponentTypeMessage(type: string): string {
+    switch (type) {
+      case 'ai':
+        return 'Jouer contre un ordinateur';
+      case 'player':
+        return 'Jouer à 2 joueurs';
+      default:
+        return 'Adversaire inconnue';
+    }
+  }
+
+  selectOpponentType(type: string) {
+    this.selected_opponent_type = type;
+  }
+
+  isSelectedOpponentType(type: string): string {
+    return this.selected_opponent_type === type ? 'selected' : '';
+  }
+
+  isOnePlayerGame(): boolean {
+    return this.selected_opponent_type === 'ai'
+  }
+  
+  /* Functions for side selection */
+
+  getSideName(side: string): string {
+    switch (side) {
+      case 'goat':
+        return 'Chèvre';
+      case 'harvest':
+        return 'Collecteur de choux';
+      default:
+        return 'Camp inconnue';
+    }
+  }
+
+  selectSide(side: string) {
+    this.player_side = side;
+  }
+
+  isSelectedSide(side: string): string {
+    return this.player_side === side ? 'selected' : '';
+  }
+
+  /* Start game function */
+
+  startGame() {
+    console.log('Starting game')
+    this.gameService.board_conf = this.selected_configuration;
+    this.gameService.opponent_type = this.selected_opponent_type;
+    this.gameService.player_side = this.player_side;
+    const params: number[] = []
+
+    this.gameService.board_params = params;
+    //this.router.navigate(['/board'])
   }
 
 }
--- WebSurfeur/src/app/services/game/game.service.ts
+++ WebSurfeur/src/app/services/game/game.service.ts
@@ -6,6 +6,11 @@ import Swal from 'sweetalert2';
 })
 export class GameService {
 
+  private _board_configuration: string | undefined;
+  private _board_params: number[] = [];
+  private _opponent_type: string | undefined;
+  private _player_side: string = 'unknown';
+
   constructor() { }
 
   // Functions use to display the rules
@@ -19,4 +24,35 @@ export class GameService {
       text: this.getRulesHtml()
     })
   }
+
+  /* Getters */
+
+  get board_conf(): string {
+    if(this._board_configuration === undefined) {
+      return 'unknwon';
+    }
+    return this._board_configuration;
+  }
+
+  get board_params(): number[] { return this._board_params; }
+
+  get opponent_type(): string {
+    if(this._opponent_type === undefined) {
+      return 'unknown';
+    }
+    return this._opponent_type;
+  }
+
+  get player_side(): string { return this._player_side; }
+
+  /* Setters */
+
+  set board_conf(conf: string) { this._board_configuration = conf; }
+
+  set board_params(params: number[]) { this._board_params = params; }
+
+  set opponent_type(type: string) { this._opponent_type = type; }
+
+  set player_side(side: string) { this._player_side = side; }
+
 }
